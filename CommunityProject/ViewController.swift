c51419b2a2c80f4d28ea54df4f67facbf125f423
yeonupark <idepix@naver.com>
2023-11-14 16:56:46 +0900

    Initial Commit

---
+++ CommunityProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommunityProject
+//
+//  Created by Yeonu Park on 2023/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
